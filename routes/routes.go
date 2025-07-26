47aa3fb5585ffbbf80ab3bad8d0a14c153ad0df3
qsw001 <wqs060204@qq.com>
2025-07-26 16:07:06 +0800

    改正书写

--- routes/routes.go
+++ routes/routes.go
@@ -17,7 +17,7 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	//注册cros，cros注册要在登陆的前面，防止登陆时就出现跨域请求失败
+	//注册cors，cors注册要在登陆的前面，防止登陆时就出现跨域请求失败
 	r.Use(middleware.CORSMiddleware())
 
 	//注册日志，注册日志也要放在前面，可以记录所有的信息
